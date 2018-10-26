471f47db7e37b5da325e97bdf4ab3c3667dd0313
banyland <banyland90@gmail.com>
2018-10-25 20:37:37 -0500

    updated models

--- models/user_info.js
+++ models/user_info.js
@@ -1,13 +1,29 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    score: DataTypes.INTEGER
+  var User = sequelize.define("User", {
+    id: {
+      type: type.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
+username: {
+  type: type.STRING,
+      allowNull: false
+},
+    email: {
+      type: type.STRING,
+      allowNull: false
+    },
+  password: {
+    type: type.STRING,
+      allowNull: false
+  },
+    score:
+      DataTypes.INTEGER
+  
   });
   User.associate = function(models) {
     User.hasMany(models.User, {
-      onDelete: 'cascade'
+      onDelete: "cascade"
     });
   };
   return User;
