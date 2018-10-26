9edfb1f9a7794e2e8557ec8e890a01b3ceee3f54
Black Jack <hibbarcd@gmail.com>
2018-10-26 16:43:27 -0500

    css styling commit

--- models/user_info.js
+++ models/user_info.js
@@ -1,29 +1,29 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
+  var User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER, 
       primaryKey: true, 
       autoIncrement: true
     },
-username: {
-  type: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
       allowNull: false
-},
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false
     },
-  password: {
-    type: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
       allowNull: false
-  },
+    },
     score:
       DataTypes.INTEGER
   
   });
   User.associate = function(models) {
     User.hasMany(models.User, {
-      onDelete: "cascade"
+      onDelete: 'cascade'
     });
   };
   return User;
